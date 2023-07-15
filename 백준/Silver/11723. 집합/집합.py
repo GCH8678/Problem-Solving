7ed97af27e1dd671ac68fce0a45b8e3e57182c5b
GCH8678 <95828987+GCH8678@users.noreply.github.com>
2023-07-16 06:22:37 +0900

    [Silver V] Title: 집합, Time: 3380 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,46 +1,27 @@
 import sys
 
-def add(s:set,x:int):
-    if x not in s:
-        s.add(x)
-
-def remove(s:set,x:int):
-    if x in s:
-        s.remove(x)
-
-def check(s:set,x:int):
-    if x in s:
-        print(1)
-    else:
-        print(0)
-
-def toggle(s:set,x:int):
-    if x in s:
-        s.remove(x)
-    else:
-        s.add(x)
-
-def all():
-    return set([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
-
-def empty():
-    return set()
 
 
 m = int(sys.stdin.readline())
-s=set()
+#s=set()
+s=[False]*20
+allS = [True]*20
+emptyS = [False]*20
 for _ in range(m):
-    fn = list(sys.stdin.readline().split())
+    fn = sys.stdin.readline().split()
 
     if fn[0] == "add":
-        add(s,int(fn[1]))
+        s[int(fn[1])-1] = True
     elif fn[0]=="remove":
-        remove(s,int(fn[1]))
+        s[int(fn[1])-1] = False
     elif fn[0]=="check":
-        check(s,int(fn[1]))
+        if s[int(fn[1])-1]:
+            print(1)
+        else:
+            print(0)
     elif fn[0]=="toggle":
-        toggle(s,int(fn[1]))
+        s[int(fn[1])-1] = not s[int(fn[1])-1]
     elif fn[0]=="all":
-        s=all()
+        s=allS
     else:
-        s=empty()
+        s=emptyS
