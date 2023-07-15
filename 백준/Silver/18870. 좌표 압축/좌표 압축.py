7d3d220ece1939751dd0899a7548b5e403ba37b5
GCH8678 <95828987+GCH8678@users.noreply.github.com>
2023-07-16 08:56:18 +0900

    [Silver II] Title: 좌표 압축, Time: 1536 ms, Memory: 302880 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,24 +1,29 @@
 #첫 째 줄 n
 # 둘쨰 줄 x1,x2,x3,...,xn 
 import sys
-
 n = int(sys.stdin.readline())
 
-lst = list(map(list,enumerate(map(int,sys.stdin.readline().split()))))
+lst = list(map(int,sys.stdin.readline().split()))
 
-# lst 값 순으로 정렬
-lst.sort(key=lambda x: x[1])
-num = lst[0][1]
-# result에 가장 작은 값을 가진 idx 넣기
-# result => (갯수, idx)
-result = [[0,lst[0][0]]]
-#lst => (idx, x)
-for i in lst[1:]:
-    if i[1]>num:
-        num = i[1]
-        result.append([result[-1][0]+1,i[0]])
-    else:
-        result.append([result[-1][0],i[0]])
-result.sort(key=lambda x: x[1])
+srt = sorted(set(lst))
+dic = {srt[i]:i for i in range(len(srt))}
+for i in lst:
+    print(dic[i], end=" ")
 
-print(*[i[0] for i in result])
+#for i in range(len(srt)):
+# # lst 값 순으로 정렬
+# lst.sort(key=lambda x: x[1])
+# num = lst[0][1]
+# # result에 가장 작은 값을 가진 idx 넣기
+# # result => (갯수, idx)
+# result = [[0,lst[0][0]]]
+# #lst => (idx, x)
+# for i in lst[1:]:
+#     if i[1]>num:
+#         num = i[1]
+#         result.append([result[-1][0]+1,i[0]])
+#     else:
+#         result.append([result[-1][0],i[0]])
+# result.sort(key=lambda x: x[1])
+
+# print(*[i[0] for i in result])
